b93ab838bfd5700cd1a8c81d3b638a0a6fbcbeaa
Luka Tutek <luka.tutek@swisscom.com>
2024-04-03 15:02:38 +0200

    feat: starting commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,31 +6,10 @@ import { IntroductionComponent } from '../../components/introduction/introductio
   standalone: true,
   imports: [IntroductionComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
-  slideIndex: number = 0;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-    this.showSlide(this.slideIndex);
-  }
-
-  showSlide(index: number) {
-    const slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slides[index].style.display = "block";
-  }
-
-  nextSlide() {
-    this.slideIndex++;
-    const slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
-    if (this.slideIndex >= slides.length) {
-      this.slideIndex = 0;
-    }
-    this.showSlide(this.slideIndex);
-  }
+  ngOnInit() {}
 }
