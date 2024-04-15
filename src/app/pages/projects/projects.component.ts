bbfd744b846852d61c803e15ca23190c45f5e1cf
Luka Tutek <luka.tutek@swisscom.com>
2024-04-15 13:19:09 +0200

    feat: projectCards have expandable buttons for technology

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -7,4 +7,74 @@ import { Component } from '@angular/core';
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
-export class ProjectsComponent {}
+export class ProjectsComponent {
+  constructor() {}
+  toggleFirstSteps() {
+    let dots = document.getElementById('dots');
+    let moreText = document.getElementById('more-firstSteps');
+    let btnText = document.getElementById('myBtn-firstSteps');
+
+    if (dots && btnText && moreText) {
+      if (dots.style.display === 'none') {
+        dots.style.display = 'inline';
+        btnText.innerHTML = 'Read more';
+        moreText.style.display = 'none';
+      } else {
+        dots.style.display = 'none';
+        btnText.innerHTML = 'Read less';
+        moreText.style.display = 'inline';
+      }
+    }
+  }
+  toogleFirstProject() {
+    let dots = document.getElementById('dots');
+    let moreText = document.getElementById('more-p1');
+    let btnText = document.getElementById('myBtn-p1');
+
+    if (dots && btnText && moreText) {
+      if (dots.style.display === 'none') {
+        dots.style.display = 'inline';
+        btnText.innerHTML = 'Read more';
+        moreText.style.display = 'none';
+      } else {
+        dots.style.display = 'none';
+        btnText.innerHTML = 'Read less';
+        moreText.style.display = 'inline';
+      }
+    }
+  }
+  toogleSecondProject() {
+    let dots = document.getElementById('dots');
+    let moreText = document.getElementById('more-p2');
+    let btnText = document.getElementById('myBtn-p2');
+
+    if (dots && btnText && moreText) {
+      if (dots.style.display === 'none') {
+        dots.style.display = 'inline';
+        btnText.innerHTML = 'Read more';
+        moreText.style.display = 'none';
+      } else {
+        dots.style.display = 'none';
+        btnText.innerHTML = 'Read less';
+        moreText.style.display = 'inline';
+      }
+    }
+  }
+  toogleThirdProject() {
+    let dots = document.getElementById('dots');
+    let moreText = document.getElementById('more-p3');
+    let btnText = document.getElementById('myBtn-p3');
+
+    if (dots && btnText && moreText) {
+      if (dots.style.display === 'none') {
+        dots.style.display = 'inline';
+        btnText.innerHTML = 'Read more';
+        moreText.style.display = 'none';
+      } else {
+        dots.style.display = 'none';
+        btnText.innerHTML = 'Read less';
+        moreText.style.display = 'inline';
+      }
+    }
+  }
+}
