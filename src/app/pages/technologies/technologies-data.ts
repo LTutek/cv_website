c1f4f081feec47d6391c6e9996e05e53bb678233
Luka Tutek <luka.tutek@swisscom.com>
2024-04-16 14:31:57 +0200

    feat: added all images of Technologies

--- src/app/pages/technologies/technologies-data.ts
+++ src/app/pages/technologies/technologies-data.ts
@@ -2,112 +2,112 @@
 export const technologies = [
   {
     name: 'Angular',
-    logo: 'assets/angular.svg',
+    logo: '../../../assets/technology/angular.svg',
     description:
       'Angular is a platform and framework for building single-page client applications using HTML and TypeScript.',
     category: 'frontend',
   },
   {
     name: 'Vue',
-    logo: 'assets/vue.svg',
+    logo: '../../../assets/technology/vue.png',
     description:
       'Vue.js is a progressive framework for building user interfaces.',
     category: 'frontend',
   },
   {
     name: 'NestJS',
-    logo: 'assets/nestjs.svg',
+    logo: '../../../assets/technology/NestJS.svg.png',
     description:
       'NestJS is a framework for building efficient, reliable and scalable server-side applications.',
     category: 'backend',
   },
   {
     name: 'MariaDB',
-    logo: 'assets/mariadb.svg',
+    logo: '../../../assets/technology/mariaDB.svg',
     description:
       'MariaDB is an open-source relational database management system, backward compatible, binary drop-in replacement of MySQL.',
     category: 'backend',
   },
   {
     name: 'MongoDB',
-    logo: 'assets/mongodb.svg',
+    logo: '../../../assets/technology/mongoDB.png',
     description:
       'MongoDB is a document-oriented NoSQL database used for high volume data storage.',
     category: 'backend',
   },
   {
     name: 'Python',
-    logo: 'assets/python.svg',
+    logo: '../../../assets/technology/python.png',
     description:
       'Python is an interpreted, high-level, general-purpose programming language.',
     category: 'backend',
   },
   {
     name: 'HTML',
-    logo: 'assets/html.svg',
+    logo: '../../../assets/technology/HTML5.png',
     description:
       'HTML (HyperText Markup Language) is the standard markup language for documents designed to be displayed in a web browser.',
     category: 'frontend',
   },
   {
     name: 'CSS/SCSS',
-    logo: 'assets/css.svg',
+    logo: '../../../assets/technology/CSS.png',
     description:
       'CSS (Cascading Style Sheets) is a style sheet language used for describing the presentation of a document written in HTML.',
     category: 'frontend',
   },
   {
     name: 'JavaScript',
-    logo: 'assets/javascript.svg',
+    logo: '../../../assets/technology/JavaScript.png',
     description:
       'JavaScript is a programming language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else.',
     category: 'frontend',
   },
   {
     name: 'TypeScript',
-    logo: 'assets/typescript.svg',
+    logo: '../../../assets/technology/Typescript_logo_2020.svg.png',
     description:
       'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.',
-    category: 'backend, frontend',
+    category: 'backend',
   },
   {
     name: 'TypeScript',
-    logo: 'assets/typescript.svg',
+    logo: '../../../assets/technology/Typescript_logo_2020.svg.png',
     description:
       'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.',
     category: 'frontend',
   },
   {
     name: 'GitHub',
-    logo: 'assets/github.svg',
+    logo: '../../../assets/technology/github.png',
     description:
       'GitHub is a platform for hosting and collaborating on Git repositories.',
     category: 'tools',
   },
   {
     name: 'GitLab',
-    logo: 'assets/gitlab.svg',
+    logo: '../../../assets/technology/gitlab.svg',
     description:
       'GitLab is a web-based DevOps lifecycle tool that provides a Git repository manager providing wiki, issue-tracking, and CI/CD pipeline features, using an open-source license.',
     category: 'tools',
   },
   {
     name: 'Docker',
-    logo: 'assets/docker.svg',
+    logo: '../../../assets/technology/docker.svg',
     description:
       'Docker is a set of platform as a service (PaaS) products that use OS-level virtualization to deliver software in packages called containers.',
     category: 'tools',
   },
   {
     name: 'Rancher Desktop',
-    logo: 'assets/rancher.svg',
+    logo: '../../../assets/technology/rancherDesktop.jpeg',
     description:
       'Rancher Desktop is an open-source tool for building, deploying, and managing containers directly on your desktop.',
     category: 'tools',
   },
   {
     name: 'Jira',
-    logo: 'assets/jira.svg',
+    logo: '../../../assets/technology/jira.svg',
     description:
       'Jira is a proprietary issue tracking product developed by Atlassian that allows bug tracking and agile project management.',
     category: 'tools',
