7467328aa955cebb547544e525baf83f77a37591
Luka Tutek <luka.tutek@swisscom.com>
2024-05-01 12:45:38 +0200

    feat: replaced contact icons with materialIcons

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
