b89913b448f56c0e33738c8d60ac6e2bac8df129
Luka Tutek <luka.tutek@swisscom.com>
2024-05-06 11:30:43 +0200

    feat: WIP

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
